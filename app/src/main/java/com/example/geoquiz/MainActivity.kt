e121202efc4a51a85d5124b52a286de69c231798
Nik <sollwar@yandex.ru>
2022-03-01 17:52:38 +0300

    Добавлена вторая activity, передача данных в неё и из неё. Intent'ы

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.geoquiz
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -13,6 +16,8 @@ import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
+private const val EXTRA_ANSWER_SHOWN = "com.example.geoquiz.answer_shown"
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button // компилятору что перед использованием элемента мы введём ненулевое значение для него
     private lateinit var nextButton: Button
     private lateinit var prevButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
     private val quizViewModel: QuizViewModel by lazy { // ленивая инициализация QuizViewModel
@@ -43,8 +49,16 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         prevButton = findViewById(R.id.prev_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
+        cheatButton.setOnClickListener {
+            //val intent = Intent(this, CheatActivity::class.java) // Обьявление явного intent для запуска activity
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            //startActivity(intent) // Старт activity
+            startActivityForResult(intent, REQUEST_CODE_CHEAT) // Старт activity "с ожиданием результата"
+        }
 
         nextButton.setOnClickListener {
             quizViewModel.moveToNext()
@@ -67,6 +81,16 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            quizViewModel.isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
+
     override fun onSaveInstanceState(savedInstanceState: Bundle) { // Сохраняем currentIndex при выполнении onPause()
         super.onSaveInstanceState(savedInstanceState)
         Log.i(TAG, "onSaveInstanceState")
@@ -74,12 +98,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        isEnableAnswerButton(false)
         val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+//        val messageResId = if (userAnswer == correctAnswer) {
+//            R.string.correct_toast
+//        } else {
+//            R.string.incorrect_toast
+//        }
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
@@ -87,11 +115,5 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
-        isEnableAnswerButton(true)
-    }
-
-    private fun isEnableAnswerButton(enable: Boolean) {
-        falseButton.isEnabled = enable
-        trueButton.isEnabled = enable
     }
 }
